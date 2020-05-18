1b0e999d018cddc4c16fa4a80ca35db421f7a43d
Alex Krakovsky <alexkrakovsky@Alexs-MacBook-Pro-2.local>
2020-05-17 18:27:41 -0700

    couldn't do cycles

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +12,50 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private Queue<Integer> fringe;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = m.xyTo1D(sourceX, sourceY);
+        t = m.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        fringe = new LinkedList<>();
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        fringe.add(s);
+        marked[s] = true;
+        announce();
+        if (s == t) {
+            return;
+        }
+        while (!fringe.isEmpty()) {
+            int c = fringe.remove();
+            for (int n : maze.adj(c)) {
+                if (!marked[n]) {
+                    edgeTo[n] = c;
+                    distTo[n] = distTo[c] + 1;
+                    marked[n] = true;
+                    announce();
+                    if (n == t) {
+                        return;
+                    }
+                    fringe.add(n);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +11,59 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Stack<Integer> fringe;
+    private Maze maze;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        fringe = new Stack<>();
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        cycleDetector();
     }
 
-    // Helper methods go here
-}
+    private void cycleDetector() {
+        fringe.push(0);
+
+        while (!fringe.isEmpty()) {
+            int v = fringe.pop();
+            marked[v] = true;
+            announce();
+            for (int n : maze.adj(v)) {
+                if (!marked[n]) {
+                    fringe.push(n);
+                } else if (checkFringe(fringe, n)) {
+
+
+                }
+
+            }
+        }
+    }
+
+        private boolean checkFringe (Stack < Integer > fringe,int x){
+            Stack<Integer> holding = new Stack<>();
+            while (!fringe.isEmpty()) {
+                int v = fringe.pop();
+                holding.push(v);
+                if (v == x) {
+                    while (!holding.isEmpty()) {
+                        fringe.push(holding.pop());
+                    }
+                    return true;
+                }
+            }
+            while (!holding.isEmpty()) {
+                fringe.push(holding.pop());
+            }
+            return false;
+        }
+    }
+
+
+
+
 
